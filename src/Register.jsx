f8e05d1aef75c96fe5ad79f1eb3f551da7bc11cf
hnanTs <phucthanh2004ct@gmail.com>
2024-11-20 10:47:51 +0700

    small bug fixed

--- src/Register.jsx
+++ src/Register.jsx
@@ -96,7 +96,6 @@ const Register = () => {
                 <option value="" disabled>Gender</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
-                <option value="other">Other</option>
               </select>
             </div>
             <div className="register-birthday-label">
